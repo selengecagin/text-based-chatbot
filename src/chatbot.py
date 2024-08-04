dcdaedc6ac0331651abe1b8cf5e504ed55a90792
selengecagin <selengecagin@gmail.com>
2024-08-04 16:33:10 +0300

    Load the pre-trained model and tokenizer

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,2 +1,8 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
+
+
+model_name = "microsoft/DialoGPT-small"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
